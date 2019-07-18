73857a17d69cad84744ea7c27a9eea8e4cbc891a
Lance <lance@daosoft.cn>
2019-07-18 18:54:49 +0800

    更新控制器

--- app/home/controller/Captcha.php
+++ app/home/controller/Captcha.php
@@ -5,14 +5,15 @@ namespace app\home\controller;
 /**
  * 生成验证码
  */
-
-
-define('INIT_NO_SMARTY', true);
-
-
-$img = new captcha(ROOT_PATH . 'data/captcha/', $_CFG['captcha_width'], $_CFG['captcha_height']);
-@ob_end_clean(); //清除之前出现的多余输入
-if (isset($_REQUEST['is_login'])) {
-    $img->session_word = 'captcha_login';
+class Captcha extends Init
+{
+    public function index()
+    {
+        $img = new captcha(ROOT_PATH . 'data/captcha/', $_CFG['captcha_width'], $_CFG['captcha_height']);
+        @ob_end_clean(); //清除之前出现的多余输入
+        if (isset($_REQUEST['is_login'])) {
+            $img->session_word = 'captcha_login';
+        }
+        $img->generate_image();
+    }
 }
-$img->generate_image();
--- app/home/controller/Region.php
+++ app/home/controller/Region.php
@@ -5,20 +5,22 @@ namespace app\home\controller;
 /**
  * 地区切换程序
  */
+class Region extends Init
+{
+    public function index()
+    {
 
 
-define('INIT_NO_USERS', true);
-define('INIT_NO_SMARTY', true);
+        header('Content-type: text/html; charset=' . EC_CHARSET);
 
+        $type = !empty($_REQUEST['type']) ? intval($_REQUEST['type']) : 0;
+        $parent = !empty($_REQUEST['parent']) ? intval($_REQUEST['parent']) : 0;
 
-header('Content-type: text/html; charset=' . EC_CHARSET);
+        $arr['regions'] = get_regions($type, $parent);
+        $arr['type'] = $type;
+        $arr['target'] = !empty($_REQUEST['target']) ? stripslashes(trim($_REQUEST['target'])) : '';
+        $arr['target'] = htmlspecialchars($arr['target']);
 
-$type = !empty($_REQUEST['type']) ? intval($_REQUEST['type']) : 0;
-$parent = !empty($_REQUEST['parent']) ? intval($_REQUEST['parent']) : 0;
-
-$arr['regions'] = get_regions($type, $parent);
-$arr['type'] = $type;
-$arr['target'] = !empty($_REQUEST['target']) ? stripslashes(trim($_REQUEST['target'])) : '';
-$arr['target'] = htmlspecialchars($arr['target']);
-
-echo json_encode($arr);
+        echo json_encode($arr);
+    }
+}
