e0da5ee8aa7f86ceb0f314003d9d5d3e150b44c2
Lance <lance@daosoft.cn>
2019-07-23 09:24:46 +0800

    优化

--- app/command/ConvertTemplate.php
+++ app/command/ConvertTemplate.php
@@ -76,22 +76,22 @@ class ConvertTemplate extends Command
              * {$user['name']} => <?php echo $user['name'];?>
              * {$user.name}    => <?php echo $user['name'];?>
              */
-            '/{(\\$[a-zA-Z_]\w*(?:\[[\w\.\"\'\[\]\$]+\])*)}/i' => "<?php echo $1; ?>",
+            /*'/{(\\$[a-zA-Z_]\w*(?:\[[\w\.\"\'\[\]\$]+\])*)}/i' => "<?php echo $1; ?>",
             '/\$(\w+)\.(\w+)\.(\w+)\.(\w+)/is' => "\$\\1['\\2']['\\3']['\\4']",
             '/\$(\w+)\.(\w+)\.(\w+)/is' => "\$\\1['\\2']['\\3']",
-            '/\$(\w+)\.(\w+)/is' => "\$\\1['\\2']",
+            '/\$(\w+)\.(\w+)/is' => "\$\\1['\\2']",*/
 
             /**
              * constance label
              * {CONSTANCE} => <?php echo CONSTANCE;?>
              */
-            '/\{([A-Z_\x7f-\xff][A-Z0-9_\x7f-\xff]*)\}/s' => "\\1/",
+            // '/\{([A-Z_\x7f-\xff][A-Z0-9_\x7f-\xff]*)\}/s' => "\\1/",
 
             /**
              * include label
              * {include file="test"}
              */
-            '/\<\!-- \#BeginLibraryItem "(.+?)" --\>.*?\<\!-- \#EndLibraryItem --\>/s' => '{include file="\\1"}',
+            '/\<\!-- \#BeginLibraryItem "(.+?)\.lbi" --\>.*?\<\!-- \#EndLibraryItem --\>/m' => '{include file="\\1"}',
 
             '/\s+heq\s+/' => '===',
             '/\s+nheq\s+/' => '!==',
@@ -106,8 +106,8 @@ class ConvertTemplate extends Command
             '/\{foreach\s+from=\$(\S+?)\s+item=(\S+?)\s+name=(\S+?)\}/' => "{foreach $\\1 as $\\3 => $\\2}",
             '/\{foreach\s+from=\$(\S+?)\s+item=(\S+?)\}/' => "{foreach $\\1 as $\\2}",
 
-            '/\{insert_scripts\s+files=\'(.+?)\'}' => '{load href="\\1" /}',
-            //'/\{insert\s+name=\'ads\'\s+id=$ads_id num=$ads_num}' => '',
+            '/\{insert_scripts\s+files=\'(.+?)\'}/is' => '{load href="\\1" /}',
+            //'/\{insert\s+name=\'ads\'\s+id=$ads_id num=$ads_num}/is' => '',
         ];
 
         foreach ($label as $key => $value) {
